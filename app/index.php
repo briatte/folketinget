dc8f77bdc55f4cd782c9436439ab282214bd2e26
François <briatte@gmail.com>
2014-08-06 16:40:15 +0200

    less code

--- app/index.php
+++ app/index.php
@@ -176,32 +176,6 @@
 </div>
 
 <script>
-function decimalAdjust(type, value, exp) {
-	// If the exp is undefined or zero...
-	if (typeof exp === 'undefined' || +exp === 0) {
-		return Math[type](value);
-	}
-	value = +value;
-	exp = +exp;
-	// If the value is not a number or the exp is not an integer...
-	if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0)) {
-		return NaN;
-	}
-	// Shift
-	value = value.toString().split('e');
-	value = Math[type](+(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp)));
-	// Shift back
-	value = value.toString().split('e');
-	return +(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp));
-}
-
-// Decimal round
-if (!Math.round10) {
-	Math.round10 = function(value, exp) {
-		return decimalAdjust('round', value, exp);
-	};
-}
-
 // Add a method to the graph model that returns an
 // object with every neighbors of a node inside:
 sigma.classes.graph.addMethod('neighbors', function(nodeId) {
