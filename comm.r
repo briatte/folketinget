cf555a03b4852a4a04c01dd1c51817cab78f426f
François <briatte@gmail.com>
2015-02-18 17:06:47 +0100

    committee sizes

--- comm.r
+++ comm.r
@@ -119,6 +119,7 @@ for(i in ls(pattern = "^net_dk")) {
   
   m = comm[, names(comm) %in% names(sp) ]
   cat(":", nrow(m), "committees", ncol(m), "MPs")
+  M = m
   
   m = t(as.matrix(m)) # sponsors in rows, committees in columns
   m = m %*% t(m) # adjacency matrix
@@ -148,6 +149,8 @@ for(i in ls(pattern = "^net_dk")) {
   
   n %e% "committee" = e$committee
   assign(i, n)
+  
+  nn %n% "committees" = as.table(rowSums(M))
   assign(paste0("co", i), nn)
   
 }
