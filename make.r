5278a351f3a94fc897ffe01fc6d699b910eb483d
François <briatte@gmail.com>
2015-08-06 18:41:23 +0200

    use raw data if found zipped

--- make.r
+++ make.r
@@ -6,10 +6,18 @@ source("parties.r")
 
 # folders
 
-dir.create("data"   , showWarnings = FALSE)
-dir.create("photos" , showWarnings = FALSE)
-dir.create("plots"  , showWarnings = FALSE)
-dir.create("raw"    , showWarnings = FALSE)
+dir.create("data"  , showWarnings = FALSE)
+dir.create("plots" , showWarnings = FALSE)
+
+if (file.exists("photos.zip"))
+  unzip("photos.zip")
+
+dir.create("photos", showWarnings = FALSE)
+
+if (file.exists("raw.zip"))
+  unzip("raw.zip")
+
+dir.create("raw", showWarnings = FALSE)
 
 # parameters
 
